ae216b2b69ca2f0fb4ed4fe6b1352bba7ddabbfa
vladik <deez1dayz@gmail.com>
2026-04-21 19:34:40 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -57,3 +57,98 @@ public:
         if (catchCallback) catchCallback();
     }
 };
+shared_ptr<SimplePromise> asyncFindPromise(
+    const vector<int>& arr,
+    function<bool(int)> predicate,
+    atomic<bool>& cancelled
+) {
+    auto promise = make_shared<SimplePromise>();
+
+    thread([=, &cancelled]() {
+        for (int i = 0; i < arr.size(); i++) {
+
+            if (cancelled) {
+                cout << "Operation cancelled (promise)\n";
+                return;
+            }
+
+            this_thread::sleep_for(chrono::milliseconds(300));
+
+            if (predicate(arr[i])) {
+                promise->resolve(arr[i]);
+                return;
+            }
+        }
+
+        promise->reject();
+        }).detach();
+
+    return promise;
+}
+
+int asyncFindAwait(
+    const vector<int>& arr,
+    function<bool(int)> predicate,
+    atomic<bool>& cancelled
+) {
+    for (int i = 0; i < arr.size(); i++) {
+
+        if (cancelled) {
+            throw runtime_error("Cancelled");
+        }
+
+        this_thread::sleep_for(chrono::milliseconds(300));
+
+        if (predicate(arr[i])) {
+            return arr[i];
+        }
+    }
+
+    throw runtime_error("Not found");
+}
+
+int main() {
+    vector<int> arr = { 1, 3, 5, 8, 10, 13 };
+    atomic<bool> cancelled(false);
+
+    cout << "=== Callback version ===\n";
+
+    asyncFindCallback(
+        arr,
+        [](int x) { return x % 2 == 0; },
+        [](int result) {
+            cout << "Found (callback): " << result << endl;
+        },
+        []() {
+            cout << "Not found (callback)\n";
+        },
+        cancelled
+    );
+
+    this_thread::sleep_for(chrono::seconds(2));
+
+    cout << "\n=== Promise version ===\n";
+
+    auto promise = asyncFindPromise(
+        arr,
+        [](int x) { return x > 9; },
+        cancelled
+    );
+    promise->then([](int result) {
+        cout << "Found (promise): " << result << endl;
+        });
+
+    promise->onError([]() {
+        cout << "Not found (promise)\n";
+        });
+
+    this_thread::sleep_for(chrono::seconds(2));
+
+    cout << "\n=== Async/Await version ===\n";
+
+    try {
+        int result = asyncFindAwait(
+            arr,
+            [](int x) { return x == 5; },
+            cancelled
+        );
